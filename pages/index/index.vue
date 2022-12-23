ae16a668d6bcc4f358b00b75ab8206ffb9e2fc57
陈乔宇 <wo5559597@163.com>
2022-12-23 15:18:55 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+   <view class="box">
+   	 <h1>零基础学uniapp</h1>
+   </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+.box{
+	height: 100px;
+	background: red;
+	h1{
+		font-size: 50px;
+		color: pink;
+	}
+}
+</style>
---
+++ pages/list/list.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h2>新闻页签</h2>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
